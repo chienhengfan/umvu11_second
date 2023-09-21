3254c4128c723054634c9434fd3e098f5dbda4f6
Jack_Lu <a0932875458@gmail.com>
2023-09-21 21:26:14 +0800

    scene and menu Jack-don't cover it

---
+++ Assets/Effects/ParticleCollisionHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleCollisionHandler : MonoBehaviour
+{
+    public ParticleSystem explosionPrefab; // 爆炸特效的预制体
+
+    private ParticleSystem explosionInstance; // 实际爆炸特效的实例
+
+    private void Start()
+    {
+        // 在开始时实例化爆炸特效，但不激活
+        explosionInstance = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        explosionInstance.gameObject.SetActive(false);
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("+++++++++++++++");
+        // 当粒子碰撞时，激活爆炸特效
+        if (explosionInstance != null)
+        {
+            explosionInstance.transform.position = transform.position; // 设置特效位置
+            explosionInstance.gameObject.SetActive(true);
+            Debug.Log("+++++++++++++++");
+        }
+        Debug.Log("+++++++++++++0000000000000000000");
+        // 禁用当前游戏对象
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/MenuManager.cs
@@ -0,0 +1,51 @@
+﻿using RPGCharacterAnims;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject menuUI;
+    private bool isPaused = false;
+    public GameObject cameraController; // 引用相机控制脚本所在的物体
+    private void Start()
+    {
+        menuUI.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToggleMenu();
+        }
+
+        // 检查是否要暂停游戏时间
+        if (isPaused)
+        {
+            Time.timeScale = 0f; // 游戏时间暂停
+            Cursor.lockState = CursorLockMode.None; // 解锁鼠标
+            Cursor.visible = true; // 显示鼠标
+        }
+        else
+        {
+            Time.timeScale = 1f; // 游戏时间正常
+            Cursor.lockState = CursorLockMode.Locked; // 锁定鼠标到屏幕中心
+            Cursor.visible = false; // 隐藏鼠标
+        }
+    }
+
+    void ToggleMenu()
+    {
+        if (menuUI != null)
+        {
+            menuUI.SetActive(!menuUI.activeSelf); // 切换菜单UI的激活状态
+            isPaused = !isPaused; // 切换暂停状态
+
+            // 切换相机控制脚本的激活状态
+            if (cameraController != null)
+            {
+                cameraController.SetActive(!isPaused);
+            }
+        }
+    }
+}
