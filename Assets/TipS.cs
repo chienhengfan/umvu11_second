befe17c410fe8e8fad555b8bbfef396c51363117
SHIKI934 <a0932875458@gmail.com>
2023-09-27 01:50:54 +0800

    Tips.

---
+++ Assets/TipS.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipS : MonoBehaviour
+{
+    public GameObject TextLog; // 可以按"E"的UI元素
+    public GameObject Text;
+    private bool isPlayerNearby = false;
+
+    void Update()
+    {
+        // 如果玩家靠近物体并按下"E"键，触发事件
+        if (isPlayerNearby)
+        {
+            //PerformInteraction();
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNearby = true;
+            if (TextLog != null)
+            {
+                TextLog.SetActive(true);
+            }
+            if (Text != null)
+            {
+                Text.SetActive(true);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNearby = false;
+            if (TextLog != null)
+            {
+                TextLog.SetActive(false);
+            }
+            if (Text != null)
+            {
+                Text.SetActive(false);
+            }
+        }
+    }
+}
