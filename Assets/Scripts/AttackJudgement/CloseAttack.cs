b71b4842864100b72fe414d517cc72dd4760b352
ChienHeng <harry1995841025@gmail.com>
2023-10-01 19:59:14 +0800

    add effect

--- Assets/Scripts/AttackJudgement/CloseAttack.cs
+++ Assets/Scripts/AttackJudgement/CloseAttack.cs
@@ -20,6 +20,7 @@ public class CloseAttack : MonoBehaviour
     void Update()
     {
         target = GameObject.FindGameObjectWithTag(targetTag);
+        JudgeAttack();
     }
 
     public bool IsInRange(float sectorAngle, float sectorRadius, GameObject attacker, GameObject target)
