1ddad8d62228b01b2f6d72e5a1e6fe36e1f6ad67
ChienHeng <harry1995841025@gmail.com>
2023-10-01 18:37:50 +0800

    trigger area

--- Assets/Scripts/Enemy/TriggerEnemy.cs
+++ Assets/Scripts/Enemy/TriggerEnemy.cs
@@ -4,20 +4,79 @@ using UnityEngine;
 
 public class TriggerEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Transform[] sprawnPoints;
+
+    private Dictionary<Transform, MobGroup> mobDic;
+    public GameObject airWall;
+    public GameObject enemy;
+    public string enemytag = "Enemy";
+    //public MobPoolManager mobPoolManager;
+
+
+
+
+    //private void Awake()
+    //{
+    //    mobPoolManager = MobPoolManager.Instance;
+    //}
+
     void Start()
     {
-        
+        enemy.SetActive(false);
+        airWall.SetActive(false);
+        mobDic = new Dictionary<Transform,MobGroup>();
+        sprawnPoints = gameObject.GetComponentsInChildren<Transform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(enemy.GetComponentInChildren<EnemyStateMachine>() == null)
+        {
+            airWall.SetActive(false);
+        }
     }
+
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        airWall.SetActive(true);
+    //        foreach(KeyValuePair<Transform, MobGroup> info in mobDic)
+    //        {
+    //            enemySprawlEffect.Play();
+    //            mobPoolManager.MobGenerate(info.Value, info.Key);
+    //        }
+
+    //    }
+    //}
 
     private void OnTriggerEnter(Collider other)
     {
-        
+
+        if (other.CompareTag("Player"))
+        {
+            airWall.SetActive(true);
+            enemy.SetActive(true);
+
+        }
     }
+
+    bool CheckChildWithName(GameObject obj, string name)
+    {
+        Transform trans = obj.transform;
+        Transform childTrans = trans.Find(name);
+        if (childTrans != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+
 }
