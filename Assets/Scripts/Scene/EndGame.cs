a32fbbd05cf7883dba83dfcb9cbfbab1635d135e
SHIKI934 <a0932875458@gmail.com>
2023-10-03 16:05:00 +0800

    Endmenu

--- Assets/Scripts/Scene/EndGame.cs
+++ Assets/Scripts/Scene/EndGame.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class EndGame : MonoBehaviour
 {
+    public GameObject Endmenu;
+
+    private void Start()
+    {
+        Endmenu.SetActive(false);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("MainMenu");
+            Endmenu.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
         }
         
     }
